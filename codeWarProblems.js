0656716c134855c58a0c987e39299561fa76df08
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 11:15:57 -0600

    Problem 10 complete

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -126,3 +126,24 @@ function summer(arr1,arr2){
     let sum = arr1.reduce((acc,cur)=>acc+cur)+arr2.reduce((acc,cur)=>acc+cur);
     return sum
 }
+
+//10
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+//
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+function famAge(arr){
+    let oldest=0;
+    let youngest=arr[0];
+    let retArr=[]
+    let diff;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>oldest){
+            oldest=arr[i];
+        }else if(arr[i]<youngest){
+            youngest=arr[i]
+        }
+    }
+    diff=oldest-youngest;
+    retArr.push(youngest,oldest,diff)
+    return retArr;
+}
