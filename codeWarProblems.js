a59bf92319568e5853a5b665c38f630c7a860db8
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 10:16:48 -0600

    Problem 6 complete

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -68,3 +68,18 @@ function typeValidation(variable, type){
    let ret = boo===type ? true:false
    return ret;
 }
+//6
+// #Triple Trouble
+//
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+//
+// Ex) Input: "aa", "bb" , "cc" => Output: "abcabc"
+//
+// Note: You can expect all of the inputs to be the same length.
+function tripCombine(str1,str2,str3){
+    let combined=[];
+    for(let i=0;i<str1.length;i++){
+        combined.push(str1[i]+str2[i]+str3[i])
+    }
+    return combined.join('')
+}
