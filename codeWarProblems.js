f8c7cb27cbe6190edc9ecd8c3ef598dc43979e56
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 11:31:52 -0600

    Completed #13

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -177,3 +177,26 @@ function doomsDayClock(arr){
 function oddGetter(arr){
     return arr.filter(x=>{return x%2!==0})
 }
+//13
+// You are given an initial 2-value array (x). You will use this to calculate a score.
+//
+// If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+//
+// Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+//
+// For example:
+//
+// if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
+function score(arr){
+    let score
+    if(typeof arr[0] === typeof arr[1] && typeof arr[0]=='number'){
+        score = arr[0]+arr[1];
+    } else if(typeof arr[0]===typeof arr[1]){
+        score = 'void'
+    } else if(typeof arr[0]=='number'){
+        score=arr[0]
+    }else{
+        score=arr[1]
+    }
+    return score;
+}
