b3da304a2bd325a1d722d5d9f05c2f6041af08ef
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 12:08:26 -0600

    Completed #26

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -247,3 +247,39 @@ function boolToString(b){
 function avg(arr){
     return arr.reduce((acc,cur)=>acc+cur);
 }
+
+
+
+
+
+
+
+
+
+//26
+// An AI has infected a text with a character!!
+//
+// This text is now fully mutated to this character.
+//
+// If the text or the character are empty, return an empty string.
+// There never will be a case when both are empty as nothing is going on!!
+//
+// The character is a string. It will always be of length 1 if it's not empty.
+//
+// Example:
+//
+// before = "abc"
+// character = "z"
+// after = "zzz"
+
+function infect(x,y){
+    let ret=[];
+    if(!x||!y){
+        return ''
+    }else{
+        for(let i =0; i<y.length;i++){
+            ret.push(x)
+        }
+    }
+    return ret.join('')
+}
