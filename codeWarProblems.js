fab3f4112519d82225c5aa7f816f7033c0b22430
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 11:23:38 -0600

    Finished problem 11

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -147,3 +147,26 @@ function famAge(arr){
     retArr.push(youngest,oldest,diff)
     return retArr;
 }
+
+//11
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+// Example
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+// Note: the result should be rounded down to the nearest integer.
+
+// Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+function doomsDayClock(arr){
+    let agesMul =arr.map(age=>{
+        return age*age;
+    }).reduce((acc,cur)=>acc+cur);
+    agesMul= Math.floor(Math.sqrt(agesMul)/2);
+    return agesMul
+}
