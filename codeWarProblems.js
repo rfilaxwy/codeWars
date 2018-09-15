252ac1bbe58d8e262180c2567ef8b3c7020c1030
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-15 11:42:00 -0600

    Completed #14

--- codeWarProblems.js
+++ codeWarProblems.js
@@ -200,3 +200,26 @@ function score(arr){
     }
     return score;
 }
+
+//14
+// My friend wants a new band name for her band. She like bands that use the formula: 'The' + a noun with first letter capitalized.
+//
+// dolphin -> The Dolphin
+//
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word like so (WITHOUT a 'The' in front):
+//
+// alaska -> Alaskalaska
+//
+// europe -> Europeurope
+//
+// Can you write a function that takes in a noun as a string, and returns her preferred band name written as a string?
+function bandName(str){
+    let bandName 
+    let bandstart =str[0].toUpperCase()+str.slice(1);
+    if(str[0]===str[str.length-1]){
+        bandName= bandstart+str;
+    } else{
+        bandName = `The ${bandstart}`;
+    }
+    return bandName;
+}
